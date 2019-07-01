f1fe29ebcd2d68c34de1852cef1bd7e4bda77d44
helloimhd <herda58@gmail.com>
2019-07-01 18:15:34 +0800

    done till triangle

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 console.log("hello script js");
 
-var clear = function() {
-    //document.getElementById("input").value = "";
-    console.log("went in")
-    input.value = "";
-    output.innerText = "";
-}
+let emojiSelected = "😱";
 
 var drawRow = function(input) {
     // get the div element to append
@@ -13,28 +8,136 @@ var drawRow = function(input) {
 
     let p = document.createElement("p");
     for (let i = 0; i < input; i++) {
-        p.textContent += "🍍";
+        console.log(i)
+        p.innerText += emojiSelected;
     }
     output.appendChild(p);
 } // end of draw row
 
 
+var clear = function() {
+    //document.getElementById("input").value = "";
+    input.value = "";
+    output.innerText = "";
+}
+
+
+var clearRow = function(input) {
+    // will give array
+    const splitInput = input.split(" ");
+    const rowNum = parseInt(splitInput[1])
+
+    const pGroup = document.querySelectorAll("p");
+    pGroup[rowNum-1].remove();
+}
+
+var drawMultipleRows = function(input) {
+    console.log("Entered multiple rows")
+    const splitInput = input.split(" ");
+    const column = parseInt(splitInput[0]);
+    const row = parseInt(splitInput[1]);
+
+    // create element
+    for (let i = 0; i < column; i++) {
+        var p = document.createElement("p");
+
+        for (let j = 0; j < row; j++) {
+            p.innerText += emojiSelected;
+        }
+
+        output.appendChild(p);
+    }
+}
+
+var drawTriangle = function(numOfRow) {
+    console.log("Entered drawTriangle");
+    console.log(numOfRow)
+
+    // create row accordingly
+    for (let i = 0; i < numOfRow; i++) {
+        var p = document.createElement("p");
+
+        // i is the number of times the emoji should appear
+        for (let j = 0; j < i+1; j++) {
+             p.innerText +=  emojiSelected
+        }
+        output.appendChild(p)
+    }
+
+}
+
+
+var drawrTriangle = function(numOfRow) {
+    console.log("Entered draw reverse triangle");
+    var stringInput = `${numOfRow}, ${numOfRow}`;
+    drawMultipleRows(stringInput)
+
+    var p = document.querySelectorAll("p");
+    // manipulate it
+    for (let i = 0; i < numOfRow; i++) {
+        p[i].innerHTML = ;
+    }
+
+
+    //console.log(numOfRow)
+
+    // // create row accordingly
+    // for (let i = 0; i < numOfRow; i++) {
+    //     var p = document.createElement("p");
+
+    //     // i is the number of times the emoji should appear
+    //     for (let j = 0; j < i+1; j++) {
+    //         p.innerHTML += emojiSelected
+    //     }
+
+    //     output.appendChild(p)
+    // }
+
+}
+
+var emojiHappened = function(currentInput){
+    // set the emoji
+    emojiSelected = currentInput;
+    emoji.style.display = "none";  // hide the current input
+    input.style.display = "";  // show back the other input
+    display("");
+};
 
 
 var inputHappened = function(currentInput){
+    // change current input
     const intInput = parseInt(currentInput);
-    // /console.log(isNaN(intInput))
+    //console.log(isNaN(intInput))
     if (isNaN(intInput) === true && currentInput === "clear") {
         clear();
 
-    } else {
+    } else if (currentInput.includes("clear")) {
+        clearRow(currentInput);
+        input.value = "";
+
+    } else if (currentInput.length === 1) {
         drawRow(intInput);
+        input.value = "";
+
+        // if input more than one character
+    } else {
+
+        const splitInput = currentInput.split(" ");
+        const firstInput = splitInput[0];
+        const secondInput = splitInput[1];
+
+        if (firstInput === "triangle") {
+            drawTriangle(parseInt(secondInput))
+        } else if (firstInput === "rtriangle") {
+            drawrTriangle(parseInt(secondInput))
+        } else {
+            drawMultipleRows(currentInput);
+        }
+
     }
 
-    // if (isNaN(intInput)) {
-    //     clear()
-    // } else {
-    //     drawRow(intInput);
-    // }
-
 };
+
+// hide the normal input
+input.style.display = "none";
+display("Enter the emoji you want to use!")
